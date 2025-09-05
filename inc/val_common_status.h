5f70d7a9203433f069d805c0fed4c0ba4edec69c
root <root@GM0R1KJ2.arm.com>
2025-09-05 14:33:40 +0000

    common_val: clarify IPA MSB behavior in comment and use 64-bit constant for offset
    - Reword comment to state MSB is applied at runtime in val_base_addr_ipa()
    - Mark VAL_NS_SHARED_REGION_IPA_OFFSET as ULL to match 64-bit arithmetic
    
    Fixes: Comment/macro contradiction about MSB/runtime vs compile-time constant

--- inc/val_common_status.h
+++ inc/val_common_status.h
@@ -19,8 +19,13 @@ typedef struct {
 
 #define TEST_STATUS_OFFSET         0
 
-//MSB is set at runtime based on ipa_width selected
-#define VAL_NS_SHARED_REGION_IPA_OFFSET 0x700000
+/*
+ * The top bit of the IPA (bit ipa_width-1) is OR'ed at runtime
+ * in val_base_addr_ipa() based on the selected ipa_width. This
+ * constant only encodes the low bits of the shared-region IPA
+ * and does not include the MSB.
+ */
+#define VAL_NS_SHARED_REGION_IPA_OFFSET 0x700000ULL
 
 void *val_base_addr_ipa(uint64_t ipa_width);
 void *val_get_shared_region_base_pa(void);
