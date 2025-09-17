36fd6b8023140b188fe5d58eaa44b684edcd5af8
jk-arm <jothikumar.mani@arm.com>
2025-09-17 14:33:55 +0530

    const: qualify write buffer for NVM write API
    - Make al_nvm_write take const void * in inc/val_common_peripherals.h
    - Propagate const to implementation and PAL/driver layers:
      - src/val_common_peripherals.c
      - f-a-acs/platform/common/inc/pal_common_val_intf.h
      - f-a-acs/platform/pal_baremetal/tgt_tfa_fvp/src/pal_driver.c
      - f-a-acs/platform/driver/inc/pal_nvm.h
      - f-a-acs/platform/driver/src/pal_nvm.c
    
    This clarifies API intent and prevents accidental modification of the caller's buffer.

--- inc/val_common_peripherals.h
+++ inc/val_common_peripherals.h
@@ -11,7 +11,7 @@
 #include "val_common.h"
 
 uint32_t val_nvm_read(uint32_t offset, void *buffer, size_t size);
-uint32_t val_nvm_write(uint32_t offset, void *buffer, size_t size);
+uint32_t val_nvm_write(uint32_t offset, const void *buffer, size_t size);
 uint32_t val_watchdog_enable(void);
 uint32_t val_watchdog_disable(void);
 
