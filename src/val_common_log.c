10967b50f630814c6670abfe567a7bed4d304830
jk-arm <jothikumar.mani@arm.com>
2025-12-16 19:12:05 +0530

    Fix val_printf null message handling

--- src/val_common_log.c
+++ src/val_common_log.c
@@ -588,11 +588,18 @@ out:
 uint32_t val_printf(print_verbosity_t verbosity, const char *msg, ...)
 {
     size_t chars_written = 0;
-    size_t len = log_strnlen_s(msg, LOG_MAX_STRING_LENGTH - 2);
+    static const char null_msg[] = "<NULL>";
+    const char *format = msg;
+    size_t len;
     static bool lastWasNewline = true;
     char formatted_msg[LOG_MAX_STRING_LENGTH];
     va_list args;
 
+    if (format == NULL) {
+        format = null_msg;
+    }
+
+    len = log_strnlen_s(format, LOG_MAX_STRING_LENGTH - 2);
     va_start(args, msg);
 
     if (verbosity >= VERBOSITY)
@@ -630,9 +637,9 @@ uint32_t val_printf(print_verbosity_t verbosity, const char *msg, ...)
             }
         }
 
-        if (len > 0 && msg[len - 1] == '\n')
+        if (len > 0 && format[len - 1] == '\n')
         {
-            val_mem_copy(formatted_msg, msg, len - 1);
+            val_mem_copy(formatted_msg, format, len - 1);
             formatted_msg[len - 1] = '\r';
             formatted_msg[len] = '\n';
             formatted_msg[len + 1] = '\0';
@@ -642,7 +649,7 @@ uint32_t val_printf(print_verbosity_t verbosity, const char *msg, ...)
         }
         else
         {
-            chars_written = val_log(msg, args);
+            chars_written = val_log(format, args);
             lastWasNewline = false;
         }
     }
